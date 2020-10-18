cdb76104c27f848cab2dae7a1cd3e4f03b3e5ca3
Sota-Watanabe <ma20112@shibaura-it.ac.jp>
2020-10-18 17:58:45 +0900

    画面調整

--- src/components/Documents.vue
+++ src/components/Documents.vue
@@ -1,31 +1,33 @@
 <template>
-  <div>
-    <v-tabs v-model="model">
-      <v-tab href="#tab-1">総合大会用の梗概(1ページ)</v-tab>
-      <v-tab href="#tab-2">最終発表用の梗概(2ページ)</v-tab>
-      <v-tab href="#tab-3">最終発表の資料</v-tab>
-      <v-tab href="#tab-4">卒業論文</v-tab>
-    </v-tabs>
-    <v-tabs-items v-model="model">
-      <v-tab-item value="tab-1">
+  <div style="text-align: center; margin: 100px 0">
+    <v-toolbar flat prominent>
+      <v-tabs v-model="tabs" center-active center fixed-tabs show-arrows>
+        <v-tab href="#tab-1">総合大会用の梗概(1ページ)</v-tab>
+        <v-tab href="#tab-2">最終発表用の梗概(2ページ)</v-tab>
+        <v-tab href="#tab-3">最終発表の資料</v-tab>
+        <v-tab href="#tab-4">卒業論文</v-tab>
+      </v-tabs>
+    </v-toolbar>
+    <v-tabs-items v-model="tabs">
+      <v-tab-item value="tab-4">
         <iframe
-          width="100%"
-          height="1200"
+          width="85%"
+          height="500"
           src="https://thesis-documents.s3-ap-northeast-1.amazonaws.com/ieice-synopsis.pdf"
         ></iframe>
       </v-tab-item>
       <v-tab-item value="tab-2">
         <iframe
-          width="100%"
-          height="1200"
+          width="85%"
+          height="500"
           src="https://thesis-documents.s3-ap-northeast-1.amazonaws.com/synopsis.pdf"
         ></iframe>
       </v-tab-item>
       <v-tab-item value="tab-3">
         <iframe
           src="https://onedrive.live.com/embed?resid=EF9A5F5BC26C037%2121277&amp;authkey=%21AFMwpXu8ZSYzNzg&amp;em=2&amp;wdAr=1.3333333333333333"
-          width="350px"
-          height="286px"
+          width="100%"
+          height="500"
           frameborder="0"
           >これは、<a target="_blank" href="https://office.com/webapps"
             >Office</a
@@ -36,11 +38,11 @@
           の埋め込み型のプレゼンテーションです。</iframe
         >
       </v-tab-item>
-      <v-tab-item value="tab-4">
+      <v-tab-item value="tab-1" style="height: 100vh">
         <iframe
-          width="100%"
-          height="1200"
-          src="https://thesis-documents.s3-ap-northeast-1.amazonaws.com/thesis.pdf"
+          width="80%"
+          height="100%"
+          src="https://thesis-documents.s3-ap-northeast-1.amazonaws.com/thesis.pdf#page=1"
         ></iframe>
       </v-tab-item>
     </v-tabs-items>
@@ -52,7 +54,11 @@ export default {
   name: "Documents",
 
   data: () => ({
-    model: "tab-1",
+    tabs: "tab-1",
   }),
 };
 </script>
+
+<style scoped>
+/* iframe */
+</style>
