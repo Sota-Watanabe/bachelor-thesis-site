bb89e11625668e8ad07bb9b91bdf9612073f5bac
Sota-Watanabe <ma20112@shibaura-it.ac.jp>
2020-10-18 23:10:05 +0900

    face_illustとmdi-school追加

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,5 +1,13 @@
 <template>
-  <header>卒業研究紹介ページ</header>
+  <v-app-bar>
+    <v-spacer></v-spacer>
+    <v-icon style="margin: 10px">mdi-school</v-icon>
+    <header>そーたの卒業研究紹介サイト</header>
+    <v-avatar size="35" style="margin: 0 10px">
+      <img src="../assets/face_illust.png" alt="Sota" />
+    </v-avatar>
+    <v-spacer></v-spacer>
+  </v-app-bar>
 </template>
 
 <script>
@@ -14,7 +22,6 @@ export default {
 
 <style scoped>
 header {
-  text-align: center;
+  font-size: 16px;
 }
 </style>
-
