0a63f7914d6ae8e6d936731f62772982f16a19ff
Sota-Watanabe <ma20112@shibaura-it.ac.jp>
2020-10-20 22:42:40 +0900

    dataを利用するように変更

--- src/components/Documents.vue
+++ src/components/Documents.vue
@@ -1,49 +1,18 @@
 <template>
   <div class="text-center">
-    <v-toolbar flat prominent max-width="1000" class="mx-auto">
-      <v-tabs v-model="tabs" center-active center fixed-tabs show-arrows>
-        <!-- <v-tab href="#tab-1">総合大会用の梗概(1ページ)</v-tab> -->
-        <v-tab href="#tab-2">最終発表の梗概</v-tab>
-        <v-tab href="#tab-4">卒業論文</v-tab>
-        <v-tab href="#tab-3">最終発表の資料</v-tab>
+    <v-toolbar prominent>
+      <v-tabs v-model="tab" center-active center fixed-tabs show-arrows>
+        <v-tabs-slider></v-tabs-slider>
+        <v-tab v-for="item in items" :key="item">
+          {{ item }}
+        </v-tab>
       </v-tabs>
     </v-toolbar>
-    <v-tabs-items v-model="tabs">
-      <v-tab-item value="tab-1" style="height: 100vh">
-        <iframe
-          width="85%"
-          height="100%"
-          src="https://thesis-documents.s3-ap-northeast-1.amazonaws.com/ieice-synopsis.pdf"
-        ></iframe>
-      </v-tab-item>
-      <v-tab-item value="tab-2" style="height: 100vh">
-        <iframe
-          width="85%"
-          height="100%"
-          src="https://thesis-documents.s3-ap-northeast-1.amazonaws.com/synopsis.pdf"
-        ></iframe>
-      </v-tab-item>
-      <v-tab-item value="tab-3" style="height: 100vh">
-        <iframe
-          src="https://onedrive.live.com/embed?resid=EF9A5F5BC26C037%2121277&amp;authkey=%21AFMwpXu8ZSYzNzg&amp;em=2&amp;wdAr=1.3333333333333333"
-          width="100%"
-          height="500"
-          frameborder="0"
-          >これは、<a target="_blank" href="https://office.com/webapps"
-            >Office</a
-          >
-          の機能を利用した、<a target="_blank" href="https://office.com"
-            >Microsoft Office</a
-          >
-          の埋め込み型のプレゼンテーションです。</iframe
-        >
-      </v-tab-item>
-      <v-tab-item value="tab-4" style="height: 100vh">
-        <iframe
-          width="80%"
-          height="100%"
-          src="https://thesis-documents.s3-ap-northeast-1.amazonaws.com/thesis.pdf#page=1"
-        ></iframe>
+    <v-tabs-items v-model="tab">
+      <v-tab-item v-for="element in elements" :key="element">
+        <v-card height="100vh" class="pt-8" prominent>
+          <iframe width="85%" height="100%" frameborder="0" v-bind:src="element" />
+        </v-card>
       </v-tab-item>
     </v-tabs-items>
   </div>
@@ -54,11 +23,16 @@ export default {
   name: "Documents",
 
   data: () => ({
-    tabs: "tab-1",
+    tab: null,
+    items: ["梗概", "卒業論文", "発表資料"],
+    elements: [
+      "https://thesis-documents.s3-ap-northeast-1.amazonaws.com/synopsis.pdf",
+      "https://thesis-documents.s3-ap-northeast-1.amazonaws.com/thesis.pdf",
+      "https://onedrive.live.com/embed?resid=EF9A5F5BC26C037%2121277&authkey=%21AFMwpXu8ZSYzNzg&em=2&wdAr=1.3333333333333333",
+    ],
   }),
 };
 </script>
 
 <style scoped>
-/* iframe */
 </style>
